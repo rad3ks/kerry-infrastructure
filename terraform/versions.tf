0db8c7ef2728d486a9ff04726b9b6f52e5a0b87b
Radosław Szeja <rad3ks@users.noreply.github.com>
2025-02-17 00:02:57 +0100

    feat: update terraform configuration

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -13,7 +13,7 @@ terraform {
     key                         = "terraform.tfstate"
     region                      = "eu-central-1"
     endpoints.s3                = "https://nbg1.your-objectstorage.com"
-    force_path_style           = true
+    use_path_style              = true
     skip_credentials_validation = true
     skip_metadata_api_check    = true
     skip_region_validation     = true
