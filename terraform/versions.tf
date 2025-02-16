73829f81163c61bd00f0cc9f82f0d13d78132e3f
Radosław Szeja <rad3ks@users.noreply.github.com>
2025-02-16 22:54:57 +0100

    feat: update terraform configuration

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -7,4 +7,15 @@ terraform {
       version = "~> 1.45.0"
     }
   }
+
+  backend "s3" {
+    bucket                      = "kerry-terraform-state"
+    key                         = "kerry/terraform.tfstate"
+    region                      = "eu-central-1"
+    endpoint                    = "https://s3.eu-central-1.hetzner.com"
+    force_path_style           = true
+    skip_credentials_validation = true
+    skip_metadata_api_check    = true
+    skip_region_validation     = true
+  }
 }
