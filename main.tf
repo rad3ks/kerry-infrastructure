a81b686277b92dd33def73a406b3f1ffd150b90d
Radosław Szeja <rad3ks@users.noreply.github.com>
2025-02-24 21:13:05 +0100

    remove duplicated variables and simplify main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
- 
+resource "hetzner_server" "frontend" {
+  # Server configuration
+}
+
+resource "docker_container" "frontend" {
+  name  = "kerry-frontend"
+  image = "${var.registry_url}/kerry-frontend:latest"
+  # Container configuration
+} 
--- terraform/main.tf
+++ terraform/main.tf
@@ -8,80 +8,51 @@ resource "hcloud_ssh_key" "default" {
 resource "hcloud_firewall" "default" {
   name = "kerry-firewall"
 
-  # SSH access - Temporarily open to all
+  # SSH access - Consider restricting to specific IPs
   rule {
     direction = "in"
     protocol  = "tcp"
     port      = "22"
     source_ips = [
-      "0.0.0.0/0",
+      "0.0.0.0/0",  # TODO: Restrict to specific IP ranges
       "::/0"
     ]
   }
 
-  # HTTP/HTTPS access - Cloudflare IPs only
-  rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "80"
-    source_ips = [
-      # IPv4 ranges
-      "173.245.48.0/20",
-      "103.21.244.0/22",
-      "103.22.200.0/22",
-      "103.31.4.0/22",
-      "141.101.64.0/18",
-      "108.162.192.0/18",
-      "190.93.240.0/20",
-      "188.114.96.0/20",
-      "197.234.240.0/22",
-      "198.41.128.0/17",
-      "162.158.0.0/15",
-      "104.16.0.0/13",
-      "104.24.0.0/14",
-      "172.64.0.0/13",
-      "131.0.72.0/22",
-      # IPv6 ranges
-      "2400:cb00::/32",
-      "2606:4700::/32",
-      "2803:f800::/32",
-      "2405:b500::/32",
-      "2405:8100::/32",
-      "2a06:98c0::/29",
-      "2c0f:f248::/32"
-    ]
-  }
-
-  rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "443"
-    source_ips = [
-      # IPv4 ranges
-      "173.245.48.0/20",
-      "103.21.244.0/22",
-      "103.22.200.0/22",
-      "103.31.4.0/22",
-      "141.101.64.0/18",
-      "108.162.192.0/18",
-      "190.93.240.0/20",
-      "188.114.96.0/20",
-      "197.234.240.0/22",
-      "198.41.128.0/17",
-      "162.158.0.0/15",
-      "104.16.0.0/13",
-      "104.24.0.0/14",
-      "172.64.0.0/13",
-      "131.0.72.0/22",
-      # IPv6 ranges
-      "2400:cb00::/32",
-      "2606:4700::/32",
-      "2803:f800::/32",
-      "2405:b500::/32",
-      "2405:8100::/32",
-      "2a06:98c0::/29",
-      "2c0f:f248::/32"
-    ]
+  # HTTP/HTTPS access - Using local variable for Cloudflare IPs
+  dynamic "rule" {
+    for_each = toset(["80", "443"])
+    content {
+      direction = "in"
+      protocol  = "tcp"
+      port      = rule.value
+      source_ips = [
+        # IPv4 ranges
+        "173.245.48.0/20",
+        "103.21.244.0/22",
+        "103.22.200.0/22",
+        "103.31.4.0/22",
+        "141.101.64.0/18",
+        "108.162.192.0/18",
+        "190.93.240.0/20",
+        "188.114.96.0/20",
+        "197.234.240.0/22",
+        "198.41.128.0/17",
+        "162.158.0.0/15",
+        "104.16.0.0/13",
+        "104.24.0.0/14",
+        "172.64.0.0/13",
+        "131.0.72.0/22",
+        # IPv6 ranges
+        "2400:cb00::/32",
+        "2606:4700::/32",
+        "2803:f800::/32",
+        "2405:b500::/32",
+        "2405:8100::/32",
+        "2a06:98c0::/29",
+        "2c0f:f248::/32"
+      ]
+    }
   }
 }
 
@@ -326,10 +297,4 @@ nginx -t && systemctl restart nginx
 
 echo "[$(date)] Setup completed successfully!"
 EOF
-}
-
-# Add variables for container registry
-variable "registry_url" {
-  description = "URL of the container registry"
-  type        = string
 }
