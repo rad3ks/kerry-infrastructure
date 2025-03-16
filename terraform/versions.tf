398ccc1ad6a63593658070e750f1cbfee96f4ce9
Radosław Szeja <rad3ks@users.noreply.github.com>
2025-03-16 14:09:14 +0100

    use terraform cloud for state storage

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -8,16 +8,10 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket                      = "kerry-terraform-state"
-    key                         = "terraform.tfstate"
-    region                      = "eu-central"
-    endpoints                   = { s3 = "https://nbg1.your-objectstorage.com" }
-    use_path_style              = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
+  cloud {
+    organization = "kerry"
+    workspaces {
+      name = "kerry-staging"
+    }
   }
 }
