34cd81112dade7456b8b8ab5264f8f6da5ec17d5
Radosław Szeja <rad3ks@users.noreply.github.com>
2025-03-16 13:13:59 +0100

    simplify staging provisioning

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "hetzner_server" "frontend" {
-  # Server configuration
-}
-
-resource "docker_container" "frontend" {
-  name  = "kerry-frontend"
-  image = "${var.registry_url}/kerry-frontend:latest"
-  # Container configuration
-} 
