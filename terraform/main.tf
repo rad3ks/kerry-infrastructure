a27b1d8e1c4063be61ca3750b8bce39d4cab1c5b
Radosław Szeja <rad3ks@users.noreply.github.com>
2025-03-16 13:33:18 +0100

    remove duplicated backend configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "kerry-terraform-state"
-    key    = "terraform.tfstate"
-    region = "eu-central-1"
-    endpoint = "https://s3.eu-central-1.hetzner.com"
-    skip_credentials_validation = true
-    skip_region_validation = true
-    skip_metadata_api_check = true
-    use_path_style = true
-    # Credentials will be provided via backend.hcl
-  }
-}
-
 # Create SSH key
 resource "hcloud_ssh_key" "default" {
   name       = "kerry-ssh-key"
